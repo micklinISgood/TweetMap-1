e424f95c4590a473456fbc70049ce51fb6dcf55b
Mick Lin <cl3469@columbia.edu>
2016-10-22 00:19:24 -0400

    clean

--- fetcher/test1.py
+++
@@ -1,1 +0,0 @@
-[{"tweetid":784778857026953216,"latitude":19.4439981,"longitude":-99.1453126,"status":"No trates como Yakult a quien te trata como Chamyto"},{"tweetid":784778886408052736,"latitude":38.45672402,"longitude":27.09710847,"status":"Çünkü Selda'nın doğum günü 💕 (@ Cresh Bar in İzmir, Türkiye w/ @burcuyvzzz @meneksedekoku) https://t.co/y0gyPH0nlb"}]
