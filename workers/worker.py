0bcae8f0bc9d492fb2ad0dccb058ee563508b8ca
Mick Lin <cl3469@columbia.edu>
2016-11-17 10:54:48 -0500

    sentiment anaylsis done

--- workers/worker.py
+++ workers/worker.py
@@ -1,35 +1,51 @@
 import boto.sqs, json, inspect, threading, logging, time
 conn = boto.sqs.connect_to_region("us-west-2")
 queue= conn.get_queue('tweet')
+
+from alchemyapi import AlchemyAPI
+alchemyapi = AlchemyAPI()
+
 lock = threading.Lock()
-# rs = q.get_messages()
 logging.basicConfig(level=logging.DEBUG,
                     format='(%(threadName)-10s) %(message)s',)
 def worker():
 	while True:
-	    pending =[]
+
 	    ret =[]
+
 	    lock.acquire()
 	    for message in queue.get_messages(1):
-	        # process message body
+
 	        # print [name for name,thing in inspect.getmembers(message)]
 	        # print message.get_body()
 	        body = json.loads(message.get_body())
 	        msg =json.loads(body["Message"])
 	        ret = msg
-	        for k in  msg:
-	        	# print k
-	        	pending.append(k["status"])
 
 	        # delete fetched msg in the critical section
-	        queue.delete_message(message)
+	        # queue.delete_message(message)
 	    lock.release()
-	    time.sleep(0.2)
+	    if ret:
+	    	for k in ret:
+				print k["status"]
+				response = alchemyapi.sentiment("text", k["status"])
+				# print response["language"]
+				print response["docSentiment"]
+				if "score" not in response["docSentiment"]:
+					k["sentiment"]=0
+				else:
+					k["sentiment"]=int(float(response["docSentiment"]["score"])*10)
+				# print "Sentiment: "+response["docSentiment"]["type"]
+		
+		print ret
+		time.sleep(0.2)
+
 
 	    '''
 	    do analysis with pending & ret[i]["sentiment"] = analyzed score
 	    socket.send(json.dump(ret)) 
 	    '''
+
 
 
 threads = []
