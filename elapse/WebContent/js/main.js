1f894f9deca69f541d4cd52e2e37823af0c3c7a2
Mick Lin <cl3469@columbia.edu>
2016-11-22 13:20:09 -0500

    parse lat

--- elapse/WebContent/js/main.js
+++ elapse/WebContent/js/main.js
@@ -292,7 +292,7 @@ function setGeoMarkers(data){
 function setMarkers(data){
   Object.keys(data).forEach(function(key) {
     // console.log(data[key])
-    var myLatLng = {lat: data[key]["latitude"], lng: data[key]["longitude"]};
+    var myLatLng = {lat: float(data[key]["latitude"]), lng: float(data[key]["longitude"])};
     var d = new Date(0);
     d.setUTCSeconds(data[key]["epoch"]);
 
